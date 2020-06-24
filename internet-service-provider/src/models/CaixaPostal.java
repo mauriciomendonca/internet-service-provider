6d6786afca706372b70eabe368cf4d8e9e6b3d9e
Mauricio Mendonça <mauriciomendonca14@gmail.com>
2020-06-24 02:24:53 +0000

    Ajustado a inserção do email no metodo send e formatada a string no metodo showInbox

--- internet-service-provider/src/models/CaixaPostal.java
+++ internet-service-provider/src/models/CaixaPostal.java
@@ -6,14 +6,40 @@ public class CaixaPostal {
     protected Email[] caixaEntrada;
     protected int totalEmailsCaixaSaida;
     protected int totalEmailsCaixaEntrada;
+    protected int indexCaixaSaida = 0;
 
     public CaixaPostal(String nomeDono, int totalEmailsCaixaSaida, int totalEmailsCaixaEntrada) {
         this.nomeDono = nomeDono;
         this.totalEmailsCaixaSaida = totalEmailsCaixaSaida;
         this.totalEmailsCaixaEntrada = totalEmailsCaixaEntrada;
+        this.caixaSaida = new Email[this.totalEmailsCaixaSaida];
     }
 
     public boolean send(Email email) {
-        return true; //Em construção
+        if (indexCaixaSaida < caixaSaida.length) {
+            caixaSaida[indexCaixaSaida] = email;
+            System.out.println("Email enviado para a caixa de saida com sucesso!");
+            indexCaixaSaida++;
+        }
+        return true;
+    }
+
+    public boolean receive(Email email) {
+        int i = 0;
+        if (i < caixaEntrada.length) {
+            caixaEntrada[i] = email;
+            System.out.println("Email enviado para a caixa de entrada com sucesso!");
+            return true;
+        } else
+            return false;
+    }
+
+    public String showInbox() {
+        String formatString = "";
+        for (Email e : caixaSaida) {
+            if (e != null)
+                formatString += "Caixa de entrada: [Remetente: " + e.getRemetente() + " Assunto: " + e.getAssunto() + " Corpo: " + e.getCorpoDoEmail() + "]\n";
+        }
+        return formatString;
     }
 }
