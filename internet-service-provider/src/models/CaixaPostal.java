ed3307b72e36c31023d2c3f70e50de97bbeb451f
Mauricio Mendonça <mauriciomendonca14@gmail.com>
2020-06-24 02:28:15 +0000

    Ajustado a insercao na caixa de entrada no metodo receive

--- internet-service-provider/src/models/CaixaPostal.java
+++ internet-service-provider/src/models/CaixaPostal.java
@@ -7,12 +7,14 @@ public class CaixaPostal {
     protected int totalEmailsCaixaSaida;
     protected int totalEmailsCaixaEntrada;
     protected int indexCaixaSaida = 0;
+    protected int indexCaixaEntrada = 0;
 
     public CaixaPostal(String nomeDono, int totalEmailsCaixaSaida, int totalEmailsCaixaEntrada) {
         this.nomeDono = nomeDono;
         this.totalEmailsCaixaSaida = totalEmailsCaixaSaida;
         this.totalEmailsCaixaEntrada = totalEmailsCaixaEntrada;
         this.caixaSaida = new Email[this.totalEmailsCaixaSaida];
+        this.caixaEntrada = new Email[this.totalEmailsCaixaEntrada];
     }
 
     public boolean send(Email email) {
@@ -25,13 +27,12 @@ public class CaixaPostal {
     }
 
     public boolean receive(Email email) {
-        int i = 0;
-        if (i < caixaEntrada.length) {
-            caixaEntrada[i] = email;
+        if (indexCaixaEntrada < caixaEntrada.length) {
+            caixaEntrada[indexCaixaEntrada] = email;
             System.out.println("Email enviado para a caixa de entrada com sucesso!");
-            return true;
-        } else
-            return false;
+            indexCaixaSaida++;
+        }
+        return true;
     }
 
     public String showInbox() {
